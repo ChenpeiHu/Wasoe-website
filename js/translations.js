a7d7927f3a77dde7ea1dfaa1b42206bdba294bbb
Chen pei Hu <43742401+ChenpeiHu@users.noreply.github.com>
2019-05-22 00:36:06 +0200

    Add files via upload

---
+++ js/translations.js
@@ -0,0 +1,194 @@
+/*function refreshElements(){
+
+	var dictionary = {
+		"nl": {
+			"Home": "Thuis",
+			"Services": "Diensten",
+			"Portfolio": "Portfolio",
+			"Why us?": "Waarom wij?",
+			"News": "Nieuws",
+			"Video": "Video",
+			"Skills": "Vaardigheden",
+			"Contact": "Contact",
+			"Support": "Hulp",
+
+			"Services": "Diensten",
+			"Effective mass communication.": "Effectieve massacommunicatie.",
+			"Horeca | Events | E-commerce.": "Horeca | Evenementen | E-commerce.",
+			"You want the moon? Just say the word, and we'll throw a lasso around it and pull it down.": "Wil je de maan? Zeg het woord, en wij gooien er een lasso omheen en halen hem neer.",
+			"We drive commerce through brand culture and an understanding of user behavior.": "Wij drijven handel door merkcultuur en begrip van gebruikersgedrag.",
+			"High quality content &amp; good story telling for brands that want to impact.": "Hoogwaardige content &amp; goed verhaal voor merken die impact willen hebben.",
+			"We connect organizations to their audience  through high impact websites.": "We verbinden organisaties met hun publiek via high-impact websites.",
+			"Rather than just bringing brands to life, we play a key role in defining what goes into a brand’s unique style and voice.": "In plaats van alleen maar merken tot leven te brengen, spelen we een sleutelrol bij het definiëren van wat in de unieke stijl en stem van een merk past.",
+			"We go as fast as you goes, but people can't always keep track, that is why we love automation.": "We gaan zo snel als u gaat, maar mensen kunnen niet altijd bijblijven, daarom houden we van automatisering.",
+			"We provide a secure &amp; private hosting service to our clients, to guarantee safer websites, Faster and better.": "Wij bieden een secure &amp; private hosting service aan onze klanten, om veiligere websites te garanderen. Sneller en beter.",
+			"We know firsthand the ins-and-outs of the industry, that means we'll expect to be getting midnight phone calls.": "We kennen eerstehands de ins-en-outs van de industrie, wat betekent dat we verwachten dat we middernacht telefoontjes zullen krijgen.",
+			"We make it simple, but significant. We bring your idea to life.": "We maken het eenvoudig, maar belangrijk. Wij brengen uw idee tot leven.",
+
+			"Portfolio": "Portfolio",
+			"WEB CATALOGUE": "WEB CATALOGUS",
+			"DESIGNS": "ONTWERPEN",
+			"Visuals &amp; Graphics": "Visueel &amp; Grafisch",
+			"Our Websites": "Onze websites",
+			"SOCIAL MEDIA": "SOCIALE MEDIA",
+			"See All": "Zie Alles",
+
+			"CRAFTERS": "BOUWERS",
+			"noun": "zelfstandig naamwoord",
+			"1. We're very comfortable at exchanging information on a large scale to a wide range of people. We do this mostly for Horeca | Events | E-commerce.": "1. We zijn erg comfortabel in het uitwisselen van informatie op grote schaal met een breed scala aan mensen. We doen dit voornamelijk voor Horeca | Evenementen | E-commerce.",
+			"2. What originally started as a 12 year old kido opening his first Facebook account is today a team of designers, marketeers, video freaks and developers whose roles range from strategy to brand content and UX Design. Whatever the format may be, their focus is to communicate a message that conveys emotion, feeling and that make communication a successful exchange with the final user.": "2. Wat oorspronkelijk begon als een 12-jarige kind die zijn eerste Facebook-account opende, is vandaag een team van ontwerpers, marketeers, videofreaks en ontwikkelaars wiens rollen variëren van strategie tot merkinhoud en UX Design. Wat het formaat ook is, hun focus is om een ​​boodschap over te brengen die emotie en gevoel overbrengt en die communicatie tot een succesvolle uitwisseling met de eindgebruiker maken.",
+			"First Facebook Account": "Eerste Facebook Account",
+			"Accepted into Art School": "Geaccepteerd voor Kunst School",
+			"Got Paid for a Gig!": "Betaald voor en Klus!",
+			"Nabusimake Was Born": "Nabusimake Werd Geboren",
+			"We Became WASOE": "We Zijn WASOE Geworden",
+			"Graduated: stage &amp; events": "Afgestuudeerd: stage &amp; events",
+			"Our First Office": "Ons Eerste Kantoor",
+			"Solid Team": "Dream Team",
+			"We Moved To Utrecht": "We Zijn Verhuist Naar Utrecht",
+
+			"STORIES": "VERHALEN",
+			"GO TO BLOG": "NAAR DE BLOG",
+
+			"OFFER": "AANBIEDING",
+			"We Don’t Find Solutions. We Create Them.": "We Vinden Geen Oplossingen. We Maken ze.",
+			"You can’t communicate an effective message unless you understand the people who you are trying to engage with. We never ask people what they like, we test it! We completely trust the data that our content generates. For that reason, we will work through your business while getting to know your target audience. When we have done this, we’ll create solutions that we know will work, but just to be sure, we’ll even test it.": "Je kunt geen effectieve boodschap communiceren tenzij je de mensen begrijpt met wie je probeert om te gaan. We vragen mensen nooit wat ze willen, we testen het! We vertrouwen volledig op de gegevens die onze inhoud genereert. Om die reden zullen we door uw bedrijf werken terwijl u uw doelgroep leert kennen. Wanneer we dit hebben gedaan, zullen we oplossingen creëren waarvan we weten dat ze werken, maar voor de zekerheid, we zullen het zelfs testen.",
+			"It’s as simple as that.": "Zo simpel is het.",
+
+			"Holistic Approach": "Holistische benadering",
+			"Data Driven": "Data Gedreven",
+			"Talent Rules": "Talent Heerst",
+			"You Hire Culture": "Jij huurt cultuur",
+			"Industry Experts": "Industrie-experts",
+			"Innovation Leaders": "Innovatie leiders",
+			"Broader Skillsets": "Bredere Skillsets",
+
+			"EXPERTISE": "EXPERTISE",
+			"Sometimes big problems are best solved with lots of small and creative solutions. We use a variety of tools and skills to communicate your message as effective as possible.": "Soms worden grote problemen het best opgelost met vele kleine en creatieve oplossingen. We gebruiken verschillende tools en vaardigheden om uw boodschap zo effectief mogelijk over te brengen.",
+
+			"Contact": "Contact",
+			"LET’S START SOMETHING NEW": "LATEN WE WAT NIEUWS BEGINNEN",
+			"SAY HELLO!": "ZEG HALLO!",
+			"Come along for an enthusiastic conversation. You are more than welcome at our office on the ": "Kom langs voor een enthousiast gesprek. U bent van harte welkom op ons kantoor aan het ",
+			"Euclideslaan 60, 3584 BN Utrecht.": "Euclideslaan 60, 3584 BN Utrecht.",
+			" Ofcourse, you can also call or email us.": " Natuurlijk kunt u ons ook bellen of e-mailen.",
+
+
+			
+
+		},
+		"en": {
+			"Thuis": "Home",
+			"Diensten": "Services",
+			"Portfolio": "Portfolio",
+			"Waarom wij?": "Why us?",
+			"Nieuws": "News",
+			"Video": "Video",
+			"Vaardigheden": "Skills",
+			"Contact": "Contact",
+			"Hulp": "Support",
+
+			"Diensten": "Services",
+			"Effectieve massacommunicatie.": "Effective mass communication.",
+			"Horeca | Evenementen | E-commerce.": "Horeca | Events | E-commerce.",
+			"Wil je de maan? Zeg het woord, en wij gooien er een lasso omheen en halen hem neer.": "You want the moon? Just say the word, and we'll throw a lasso around it and pull it down.",
+			"Wij drijven handel door merkcultuur en begrip van gebruikersgedrag.": "We drive commerce through brand culture and an understanding of user behavior.",
+			"Hoogwaardige content &amp; goed verhaal voor merken die impact willen hebben.": "High quality content &amp; good story telling for brands that want to impact.",
+			"We verbinden organisaties met hun publiek via high-impact websites.": "We connect organizations to their audience  through high impact websites.",
+			"In plaats van alleen maar merken tot leven te brengen, spelen we een sleutelrol bij het definiëren van wat in de unieke stijl en stem van een merk past.": "Rather than just bringing brands to life, we play a key role in defining what goes into a brand’s unique style and voice.",
+			"We gaan zo snel als u gaat, maar mensen kunnen niet altijd bijblijven, daarom houden we van automatisering.": "We go as fast as you goes, but people can't always keep track, that is why we love automation.",
+			"Wij bieden een secure &amp; private hosting service aan onze klanten, om veiligere websites te garanderen. Sneller en beter.": "We provide a secure &amp; private hosting service to our clients, to guarantee safer websites, Faster and better.",
+			"We kennen eerstehands de ins-en-outs van de industrie, wat betekent dat we verwachten dat we middernacht telefoontjes zullen krijgen.": "We know firsthand the ins-and-outs of the industry, that means we'll expect to be getting midnight phone calls.",
+			"We maken het eenvoudig, maar belangrijk. Wij brengen uw idee tot leven.": "We make it simple, but significant. We bring your idea to life.",
+
+			"Portfolio": "Portfolio",
+			"WEB CATALOGUS": "WEB CATALOGUE",
+			"ONTWERPEN": "DESIGNS",
+			"Visueel &amp; Grafisch": "Visuals &amp; Graphics",
+			"Onze websites": "Our Websites",
+			"SOCIALE MEDIA": "SOCIAL MEDIA",
+			"Zie Alles": "See All",
+
+			"BOUWERS": "CRAFTERS",
+			"zelfstandig naamwoord": "noun",
+			"1. We zijn erg comfortabel in het uitwisselen van informatie op grote schaal met een breed scala aan mensen. We doen dit voornamelijk voor Horeca | Evenementen | E-commerce.": "1. We're very comfortable at exchanging information on a large scale to a wide range of people. We do this mostly for Horeca | Events | E-commerce.",
+			"2. Wat oorspronkelijk begon als een 12-jarige kind die zijn eerste Facebook-account opende, is vandaag een team van ontwerpers, marketeers, videofreaks en ontwikkelaars wiens rollen variëren van strategie tot merkinhoud en UX Design. Wat het formaat ook is, hun focus is om een ​​boodschap over te brengen die emotie en gevoel overbrengt en die communicatie tot een succesvolle uitwisseling met de eindgebruiker maken.": "2. What originally started as a 12 year old kido opening his first Facebook account is today a team of designers, marketeers, video freaks and developers whose roles range from strategy to brand content and UX Design. Whatever the format may be, their focus is to communicate a message that conveys emotion, feeling and that make communication a successful exchange with the final user.",
+			"Eerste Facebook Account": "First Facebook Account",
+			"Geaccepteerd voor Kunst School": "Accepted into Art School",
+			"Betaald voor en Klus!": "Got Paid for a Gig!",
+			"Nabusimake Werd Geboren": "Nabusimake Was Born",
+			"We Zijn WASOE Geworden": "We Became WASOE",
+			"Afgestuudeerd: stage &amp; events": "Graduated: stage &amp; events",
+			"Ons Eerste Kantoor": "Our First Office",
+			"Dream Team": "Solid Team",
+			"We Zijn Verhuist Naar Utrecht": "We Moved To Utrecht",
+
+
+			"VERHALEN": "STORIES",
+			"NAAR DE BLOG": "GO TO BLOG",
+
+			"AANBIEDING": "OFFER",
+			"We Vinden Geen Oplossingen. We Maken ze.": "We Don’t Find Solutions. We Create Them.",
+			"Je kunt geen effectieve boodschap communiceren tenzij je de mensen begrijpt met wie je probeert om te gaan. We vragen mensen nooit wat ze willen, we testen het! We vertrouwen volledig op de gegevens die onze inhoud genereert. Om die reden zullen we door uw bedrijf werken terwijl u uw doelgroep leert kennen. Wanneer we dit hebben gedaan, zullen we oplossingen creëren waarvan we weten dat ze werken, maar voor de zekerheid, we zullen het zelfs testen.": "You can’t communicate an effective message unless you understand the people who you are trying to engage with. We never ask people what they like, we test it! We completely trust the data that our content generates. For that reason, we will work through your business while getting to know your target audience. When we have done this, we’ll create solutions that we know will work, but just to be sure, we’ll even test it.",
+			"Zo simpel is het.": "It’s as simple as that.",
+
+			"Holistische benadering": "Holistic Approach",
+			"Data Gedreven": "Data Driven",
+			"Talent Heerst": "Talent Rules",
+			"Jij huurt cultuur": "You Hire Culture",
+			"Industrie-experts": "Industry Experts",
+			"Innovatie leiders": "Innovation Leaders",
+			"Bredere Skillsets": "Broader Skillsets",
+
+
+			"EXPERTISE": "EXPERTISE",
+			"Soms worden grote problemen het best opgelost met vele kleine en creatieve oplossingen. We gebruiken verschillende tools en vaardigheden om uw boodschap zo effectief mogelijk over te brengen.": "Sometimes big problems are best solved with lots of small and creative solutions. We use a variety of tools and skills to communicate your message as effective as possible.",
+
+			"Contact": "Contact",
+			"LATEN WE WAT NIEUWS BEGINNEN": "LET’S START SOMETHING NEW",
+			"ZEG HALLO!": "SAY HELLO!",
+			"Kom langs voor een enthousiast gesprek. U bent van harte welkom op ons kantoor aan het ": "Come along for an enthusiastic conversation. You are more than welcome at our office on the ",
+			"Euclideslaan 60, 3584 BN Utrecht.": "Euclideslaan 60, 3584 BN Utrecht.",
+			" Natuurlijk kunt u ons ook bellen of e-mailen.": " Ofcourse, you can also call or email us.",
+
+
+
+		}
+	};
+
+	var currentLanguage = window.currentLanguage;
+	
+	jQuery('body').find('.translate').each(function(index, elm) {
+		var translatedString = dictionary[currentLanguage][elm.innerHTML];
+		if (translatedString === undefined) return;
+		elm.innerHTML = dictionary[currentLanguage][elm.innerHTML];
+	});
+}
+
+jQuery('a').on("click", function() {
+	if(jQuery('a').attr('href') === "http://vimfi.com/index.html?lang=en") { 
+   		sessionStorage.setItem("language", "en");
+   	} else if(jQuery('a').attr('href') === "http://vimfi.com/index.html?lang=nl") {
+   		sessionStorage.setItem("language", "nl");
+   	}else{
+   		sessionStorage.setItem("language", "es");
+   	}
+});
+
+function switchLanguage(lang) {
+	sessionStorage.setItem("language", lang);
+
+	var languagePref = sessionStorage.getItem("language");
+	window.currentLanguage = languagePref;
+	refreshElements();
+}
+
+function checkLanguage(){
+	var chosenLanguage = sessionStorage.getItem("language");
+	window.currentLanguage = chosenLanguage;
+	refreshElements();
+}
+
+window.onload = checkLanguage;
+
+*/
